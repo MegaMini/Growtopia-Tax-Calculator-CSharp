1fd5187bb522faf7c2482d33c7cfddc2014dcbb4
MegaMini <87492836+MegaMini@users.noreply.github.com>
2022-11-18 19:01:54 -0800

    Add files via upload

---
+++ Growtopia-Tax-Calculator/Growtopia-Tax-Calculator/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Growtopia_Tax_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.taxAmount = new System.Windows.Forms.TextBox();
+            this.totalBet = new System.Windows.Forms.TextBox();
+            this.result1 = new System.Windows.Forms.Label();
+            this.result2 = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(115, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate Tax";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tax Amount %:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Total Bet (wls):";
+            // 
+            // taxAmount
+            // 
+            this.taxAmount.Location = new System.Drawing.Point(189, 12);
+            this.taxAmount.Multiline = true;
+            this.taxAmount.Name = "taxAmount";
+            this.taxAmount.Size = new System.Drawing.Size(81, 49);
+            this.taxAmount.TabIndex = 4;
+            // 
+            // totalBet
+            // 
+            this.totalBet.Location = new System.Drawing.Point(189, 89);
+            this.totalBet.Multiline = true;
+            this.totalBet.Name = "totalBet";
+            this.totalBet.Size = new System.Drawing.Size(81, 49);
+            this.totalBet.TabIndex = 5;
+            // 
+            // result1
+            // 
+            this.result1.AutoSize = true;
+            this.result1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result1.Location = new System.Drawing.Point(17, 156);
+            this.result1.Name = "result1";
+            this.result1.Size = new System.Drawing.Size(0, 16);
+            this.result1.TabIndex = 6;
+            // 
+            // result2
+            // 
+            this.result2.AutoSize = true;
+            this.result2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result2.Location = new System.Drawing.Point(17, 200);
+            this.result2.Name = "result2";
+            this.result2.Size = new System.Drawing.Size(0, 16);
+            this.result2.TabIndex = 7;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(356, -1);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(33, 30);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 333);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.result2);
+            this.Controls.Add(this.result1);
+            this.Controls.Add(this.totalBet);
+            this.Controls.Add(this.taxAmount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox taxAmount;
+        private System.Windows.Forms.TextBox totalBet;
+        private System.Windows.Forms.Label result1;
+        private System.Windows.Forms.Label result2;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Growtopia-Tax-Calculator/Growtopia-Tax-Calculator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Growtopia_Tax_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int totalBetAmount = int.Parse(totalBet.Text) * 2;
+            double taxPercentage = double.Parse(taxAmount.Text, System.Globalization.CultureInfo.InvariantCulture);
+            var result = Math.Round(totalBetAmount - ((taxPercentage / 100) * totalBetAmount));
+            var youGet = totalBetAmount - result;
+
+            result1.Text = $"You give winner {result} wls";
+            result2.Text = $"You get {youGet} wls";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
